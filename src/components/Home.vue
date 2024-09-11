7eb055f0cbdaff0416d8ac27822ff90eddb47e21
Theo Henrique <102774248+theohenrique222@users.noreply.github.com>
2024-09-10 23:18:21 -0300

    commit de criação de loop nos titulos da tabela, e adição campo de busca

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,35 +5,30 @@
         <h1 class="text-4xl font-extrabold text-gray-800 mb-8 text-center">
           Tabela de Preço
         </h1>
+        
+        <div class="mb-6">
+          <input 
+            type="text" 
+            v-model="searchQuery" 
+            placeholder="Buscar" 
+            class="w-1/2 p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring focus:border-blue-300"
+          />
+        </div>
+
         <div class="min-w-full shadow-lg rounded-lg overflow-hidden">
           <table class="min-w-full leading-normal bg-white rounded-lg">
             <thead>
               <tr class="bg-gray-100">
-                <th
+                <th v-for="table in tables" :key="table.id"
                   class="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
                 >
-                  Nome
-                </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-                >
-                  Descrição
-                </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-                >
-                  Preço
-                </th>
-                <th
-                  class="px-5 py-3 border-b-2 border-gray-200 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider"
-                >
-                  Última Atualização
+                  {{ table.title }}
                 </th>
               </tr>
             </thead>
             <tbody>
               <tr
-                v-for="(item, index) in services"
+                v-for="(item, index) in filteredServices"
                 :key="index"
                 class="hover:bg-gray-50 transition-colors duration-200"
               >
@@ -73,7 +68,25 @@ export default {
   data() {
     return {
       services: [],
+      searchQuery: "", 
+      tables: [
+        { id: 1, title: "Nome" },
+        { id: 2, title: "Descrição" },
+        { id: 3, title: "Valor" },
+        { id: 4, title: "Última atualização" },
+      ]
     };
+  },
+  computed: {
+    filteredServices() {
+      return this.services.filter(service => {
+        const searchLower = this.searchQuery.toLowerCase();
+        return (
+          service.name.toLowerCase().includes(searchLower) ||
+          service.description.toLowerCase().includes(searchLower)
+        );
+      });
+    }
   },
   methods: {
     fetchServices() {
